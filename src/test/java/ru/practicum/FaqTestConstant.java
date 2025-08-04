98ce26238a170190d99955ff4ae586ede5fb5be3
Alexey Kaseka <alexeykaseka@gmail.com>
2025-08-04 19:55:33 +0300

    chande constant name

--- src/test/java/ru/practicum/FaqTestConstant.java
+++ src/test/java/ru/practicum/FaqTestConstant.java
@@ -2,32 +2,32 @@ package ru.practicum;
 
 public class FaqTestConstant {
     // Индекс вопроса
-    public static final int QUESTION_INDEX_1 = 0;
-    public static final int QUESTION_INDEX_2 = 1;
-    public static final int QUESTION_INDEX_3 = 2;
-    public static final int QUESTION_INDEX_4 = 3;
-    public static final int QUESTION_INDEX_5 = 4;
-    public static final int QUESTION_INDEX_6 = 5;
-    public static final int QUESTION_INDEX_7 = 6;
-    public static final int QUESTION_INDEX_8 = 7;
+    public static final int INDEX_QUESTION_SCOOTER_PRICE = 0;
+    public static final int INDEX_QUESTION_SEVERAL_SCOOTER = 1;
+    public static final int INDEX_QUESTION_RENT_TIME = 2;
+    public static final int INDEX_QUESTION_TODAY_ORDER = 3;
+    public static final int INDEX_QUESTION_EXTEND_RENT = 4;
+    public static final int INDEX_QUESTION_SCOOTER_CHARGING = 5;
+    public static final int INDEX_QUESTION_CANCEL_ORDER = 6;
+    public static final int INDEX_QUESTION_SCOOTER_DELIVERY = 7;
     // Текст вопроса
-    public static final String QUESTION_1 = "Сколько это стоит? И как оплатить?";
-    public static final String QUESTION_2 = "Хочу сразу несколько самокатов! Так можно?";
-    public static final String QUESTION_3 = "Как рассчитывается время аренды?";
-    public static final String QUESTION_4 = "Можно ли заказать самокат прямо на сегодня?";
-    public static final String QUESTION_5 = "Можно ли продлить заказ или вернуть самокат раньше?";
-    public static final String QUESTION_6 = "Вы привозите зарядку вместе с самокатом?";
-    public static final String QUESTION_7 = "Можно ли отменить заказ?";
-    public static final String QUESTION_8 = "Я жизу за МКАДом, привезёте?";
+    public static final String QUESTION_SCOOTER_PRICE = "Сколько это стоит? И как оплатить?";
+    public static final String QUESTION_SEVERAL_SCOOTER = "Хочу сразу несколько самокатов! Так можно?";
+    public static final String QUESTION_RENT_TIME = "Как рассчитывается время аренды?";
+    public static final String QUESTION_TODAY_ORDER = "Можно ли заказать самокат прямо на сегодня?";
+    public static final String QUESTION_EXTEND_RENT = "Можно ли продлить заказ или вернуть самокат раньше?";
+    public static final String QUESTION_SCOOTER_CHARGING = "Вы привозите зарядку вместе с самокатом?";
+    public static final String QUESTION_CANCEL_ORDER = "Можно ли отменить заказ?";
+    public static final String QUESTION_SCOOTER_DELIVERY = "Я жизу за МКАДом, привезёте?";
     // Текст вопроса
-    public static final String ANSWER_1 = "Сутки — 400 рублей. Оплата курьеру — наличными или картой.";
-    public static final String ANSWER_2 = "Пока что у нас так: один заказ — один самокат. Если хотите покататься с друзьями, можете просто сделать несколько заказов — один за другим.";
-    public static final String ANSWER_3 = "Допустим, вы оформляете заказ на 8 мая. Мы привозим самокат 8 мая в течение дня. Отсчёт времени аренды начинается с момента, когда вы оплатите заказ курьеру. Если мы привезли самокат 8 мая в 20:30, суточная аренда закончится 9 мая в 20:30.";
-    public static final String ANSWER_4 = "Только начиная с завтрашнего дня. Но скоро станем расторопнее.";
-    public static final String ANSWER_5 = "Пока что нет! Но если что-то срочное — всегда можно позвонить в поддержку по красивому номеру 1010.";
-    public static final String ANSWER_6 = "Самокат приезжает к вам с полной зарядкой. Этого хватает на восемь суток — даже если будете кататься без передышек и во сне. Зарядка не понадобится.";
-    public static final String ANSWER_7 = "Да, пока самокат не привезли. Штрафа не будет, объяснительной записки тоже не попросим. Все же свои.";
-    public static final String ANSWER_8 = "Да, обязательно. Всем самокатов! И Москве, и Московской области.";
+    public static final String ANSWER_SCOOTER_PRICE = "Сутки — 400 рублей. Оплата курьеру — наличными или картой.";
+    public static final String ANSWER_SEVERAL_SCOOTER = "Пока что у нас так: один заказ — один самокат. Если хотите покататься с друзьями, можете просто сделать несколько заказов — один за другим.";
+    public static final String ANSWER_RENT_TIME = "Допустим, вы оформляете заказ на 8 мая. Мы привозим самокат 8 мая в течение дня. Отсчёт времени аренды начинается с момента, когда вы оплатите заказ курьеру. Если мы привезли самокат 8 мая в 20:30, суточная аренда закончится 9 мая в 20:30.";
+    public static final String ANSWER_TODAY_ORDER = "Только начиная с завтрашнего дня. Но скоро станем расторопнее.";
+    public static final String ANSWER_EXTEND_RENT = "Пока что нет! Но если что-то срочное — всегда можно позвонить в поддержку по красивому номеру 1010.";
+    public static final String ANSWER_SCOOTER_CHARGING = "Самокат приезжает к вам с полной зарядкой. Этого хватает на восемь суток — даже если будете кататься без передышек и во сне. Зарядка не понадобится.";
+    public static final String ANSWER_CANCEL_ORDER = "Да, пока самокат не привезли. Штрафа не будет, объяснительной записки тоже не попросим. Все же свои.";
+    public static final String ANSWER_SCOOTER_DELIVERY = "Да, обязательно. Всем самокатов! И Москве, и Московской области.";
 
 
 }
